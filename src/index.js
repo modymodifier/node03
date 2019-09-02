9202d4673fa997ebd116e60bf045a984494346a4
mody <modymodifier@gmail.com>
2019-09-02 15:04:21 +0700

    init project

---
+++ src/index.js
@@ -0,0 +1,27 @@
+//console.log('Hello')
+
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    const url = req.url
+
+    switch (url) {
+        case '/':
+            res.end('<h1>Home Page</h1>')
+            break;
+        case 'about':
+                res.end('<h1>About Page</h1>')
+            break;
+        default:
+                res.end('<h1>Page Not Found</h1>')
+            break;
+    }
+
+    console.log(url)
+    //res.end('<h1>Hello Mody Modifier</h1>')
+})
+
+//server.listen(3000)
+
+const port = process.env.PORT || 3000
+server.listen(port)
