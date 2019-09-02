5aad5c69d05784e91b80c8da285d2e8a8925b80c
mody <modymodifier@gmail.com>
2019-09-02 15:50:46 +0700

    fix path

--- src/index.js
+++ src/index.js
@@ -9,14 +9,14 @@ const server = http.createServer((req, res) => {
         case '/':
             res.end('<h1>Home Page</h1>')
             break;
-        case 'about':
-                res.end('<h1>About Page</h1>')
+        case '/about':
+            res.end('<h1>About Page</h1>')
             break;
-        case 'contact':
-                res.end('<h1>Contact Page</h1>')
+        case '/contact':
+            res.end('<h1>Contact Page</h1>')
             break;
         default:
-                res.end('<h1>Page Not Found</h1>')
+            res.end('<h1>Page Not Found</h1>')
             break;
     }
 
